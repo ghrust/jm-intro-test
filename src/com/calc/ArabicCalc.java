a0f22db087002949aeea7e92cd2509f9208d8ca0
Rustam Garaev <garaev.rustam.h@gmail.com>
2021-03-19 06:24:48 +0400

    imort ArabicCalc

--- src/com/calc/ArabicCalc.java
+++ src/com/calc/ArabicCalc.java
@@ -1,4 +1,19 @@
 package com.calc;
 
 public class ArabicCalc {
+//    Arabic calculator. Work with arabic numbers from 1 to 10.
+
+    String operation;
+
+    ArabicCalc(String operation) {
+        this.operation = operation;
+    }
+
+    int calculate(String operation) {
+        return 0;
+    }
+
+    void displayResult() {
+        System.out.println(operation);
+    }
 }
--- src/com/calc/Main.java
+++ src/com/calc/Main.java
@@ -2,15 +2,21 @@ package com.calc;
 
 import java.util.Scanner;
 
+import com.calc.ArabicCalc;
+
 public class Main {
 
     public static void main(String[] args) {
         System.out.println("Simple Calculator");
+//        Input operation.
         Scanner in = new Scanner(System.in);
         System.out.println("Input an operation: ");
-        String number = in.nextLine();
+        String operation = in.nextLine();
 
-        System.out.printf("Your operation: %s \n", number);
+        ArabicCalc calc = new ArabicCalc(operation);
+        calc.displayResult();
+
+        System.out.printf("Your operation: %s \n", operation);
         in.close();
     }
 }
