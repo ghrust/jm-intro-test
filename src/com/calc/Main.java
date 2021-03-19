1cf5688f1d9a0d9e3aa9f24b1d2db1f9a3ef8fc4
Rustam Garaev <garaev.rustam.h@gmail.com>
2021-03-19 05:46:59 +0400

    input operatiion

--- src/com/calc/Main.java
+++ src/com/calc/Main.java
@@ -1,8 +1,16 @@
 package com.calc;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
         System.out.println("Simple Calculator");
+        Scanner in = new Scanner(System.in);
+        System.out.println("Input an operation: ");
+        String number = in.nextLine();
+
+        System.out.printf("Your operation: %s \n", number);
+        in.close();
     }
 }
